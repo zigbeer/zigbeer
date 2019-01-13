1877336a728c1aace3dc089c7e88eae0cd860c48
qm3ster <qm3ster@gmail.com>
2019-01-13 20:43:09 +0200

    Furter refactoring
    Eliminated dependencies

--- src/enum.ts
+++ src/enum.ts
@@ -1,19 +1,19 @@
-interface EnumItem<T> {
-  key: string
+export interface EnumItem<K, T> {
+  key: K
   value: T
 }
 
-export = class Enum<T> {
-  byKey: Map<string, EnumItem<T>>
-  byValue: Map<T, EnumItem<T>>
-  enums: EnumItem<T>[]
-  constructor(obj: { [key: string]: T }) {
+export class Enum<K extends string, T> {
+  byKey: Map<string, EnumItem<K, T>>
+  byValue: Map<T, EnumItem<K, T>>
+  enums: EnumItem<K, T>[]
+  constructor(obj: Record<K, T>) {
     const byKey = new Map()
     const byValue = new Map()
     const enums = []
     for (const key in obj) {
       const value = obj[key]
-      const item = { key: key, value }
+      const item = { key, value }
       byKey.set(key, item)
       byValue.set(value, item)
       enums.push(item)
