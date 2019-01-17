0d126fea3422035deab49fa23041a64b59494e26
qm3ster <qm3ster@gmail.com>
2019-01-17 20:04:22 +0200

    Add constructor
    Old singleton import can be achieved with `require("zcl-id/legacy")`

--- src/legacy.ts
+++ src/legacy.ts
@@ -1,6 +1,6 @@
-export { ZclID } from "./zcl-id"
+import { ZclID } from "./zcl-id"
 
 import * as common from "./definitions/common.json"
 import * as clusterDefs from "./definitions/cluster_defs.json"
 
-export { common, clusterDefs }
+module.exports = new ZclID(common, clusterDefs)
