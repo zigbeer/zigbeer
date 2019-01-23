3b1ebd950224ae3f642f94a33d958439cc77e61f
qm3ster <qm3ster@gmail.com>
2019-01-23 19:58:11 +0200

    Export the 3 new jsons in index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { ZclID } from "./zcl-id"
 
-import * as common from "./definitions/common.json"
-import * as clusterDefs from "./definitions/cluster_defs.json"
+import * as common from "./definitions/_common.json"
+import * as foundation from "./definitions/_foundation.json"
+import * as clusterDefs from "./definitions/_cluster_defs.json"
 
-export { common, clusterDefs }
+export { common, foundation, clusterDefs }
