48f68a9d8ea52af8da3124291f62fc64e5d769e9
qm3ster <qm3ster@gmail.com>
2019-01-30 01:40:39 +0200

    Move zclmeta.ts from zcl-packet

--- src/legacy.ts
+++ src/legacy.ts
@@ -1,7 +1,3 @@
-import { ZclID } from "./zcl-id"
+import { ZclID, common, foundation, clusterDefs } from "./"
 
-import * as common from "./definitions/_common.json"
-import * as foundation from "./definitions/_foundation.json"
-import * as clusterDefs from "./definitions/_cluster_defs.json"
-
-module.exports = new ZclID(common, foundation as any, clusterDefs as any)
+module.exports = new ZclID(common, foundation, clusterDefs)
--- test/zclId.ts
+++ test/zclId.ts
@@ -1,7 +1,3 @@
-import { ZclID } from "../src/zcl-id"
+import { ZclID, common, foundation, clusterDefs } from "../src"
 
-import * as common from "../src/definitions/_common.json"
-import * as foundation from "../src/definitions/_foundation.json"
-import * as clusterDefs from "../src/definitions/_cluster_defs.json"
-
-export const zclId = new ZclID(common, foundation as any, clusterDefs as any)
+export const zclId = new ZclID(common, foundation, clusterDefs)
