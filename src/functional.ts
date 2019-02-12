8bf26c0e20ed3198b4ec93f211f798aa28589784
qm3ster <qm3ster@gmail.com>
2019-02-12 03:32:05 +0200

    Get rid of factories
    Also allows emitting declarations

--- src/functional.ts
+++ src/functional.ts
@@ -6,7 +6,7 @@ import { writeDataTable } from "./writeDataTypes"
 import { readDataTable } from "./readDataTypes"
 import { getStdType } from "./definition"
 
-type ParamTypes = keyof (typeof frameArgMap & typeof writeDataTable)
+type ParamTypes = keyof (typeof specialWrites & typeof writeDataTable)
 
 const specialReads = {
   preLenUint8: (r: BufferWithPointer) => r.uint8(), // TODO: Remove this once gone from zcl-id
@@ -112,165 +112,161 @@ const specialReads = {
   }
 }
 
-function funcPayloadFactory(zclId: ZclID) {
-  class FuncPayload {
-    direction: string
-    cluster: string
-    cmd: string
-    cmdId: number
-    params: [string, ParamTypes][]
-    constructor(
-      clusterId: string | number,
-      direction: 0 | 1,
-      cmd: string | number
-    ) {
-      const cluster = zclId.cluster(clusterId)
+export class FuncPayload {
+  direction: string
+  cluster: string
+  cmd: string
+  cmdId: number
+  params: [string, ParamTypes][]
+  constructor(
+    clusterId: string | number,
+    direction: 0 | 1,
+    cmd: string | number,
+    zclId: ZclID
+  ) {
+    const cluster = zclId.cluster(clusterId)
 
-      if (!cluster) {
-        throw new Error(`Unknown cluster ${clusterId}`)
-      }
-
-      this.cluster = cluster.key
-
-      const cmdEntry = direction
-        ? zclId.getCmdRsp(this.cluster, cmd)
-        : zclId.functional(this.cluster, cmd)
-      if (!cmdEntry) throw new Error(`Unknown command ${this.cluster}/${cmd}`)
-
-      this.cmd = cmdEntry.key
-
-      const wholeCommand = zclId.zclmeta.functional.get(this.cluster, this.cmd)
-      if (!wholeCommand)
-        throw new Error(`Unknown command ${this.cluster}/${this.cmd}`)
-      this.cmdId = wholeCommand.id
-      this.params = wholeCommand.params as [string, ParamTypes][]
-
-      const _direction = zclId.zclmeta.functional.getDirection(
-        this.cluster,
-        this.cmd
-      )
-      if (!_direction) {
-        throw new Error("Unrecognized direction")
-      }
-      if (_direction !== (direction ? "serverToClient" : "clientToServer")) {
-        throw new Error("Wrong direction")
-      }
-      this.direction = _direction
+    if (!cluster) {
+      throw new Error(`Unknown cluster ${clusterId}`)
     }
 
-    parse(r: BufferWithPointer) {
-      const data: Record<string, any> = {}
-      for (let [name, type] of this.params) {
-        // TODO: Remove all these dirty hacks
-        switch (type) {
-          case "dynUint8":
-            switch (name) {
-              case "scenelist":
-              case "zoneidlist":
-              case "zoneid":
-              case "bypassresult":
-                r.fwd(-1)
-                type = "len8uint8" as any
-                break
-              case "logpayload":
-                r.fwd(-4)
-                type = "len32uint8" as any
-                break
-              case "intervals":
-                r.fwd(-3) // len8 and attrId
-                type = "intervals" as any
-                break
-              default:
-                throw new Error(`unknown dynUint8 name: ${name}`)
-            }
-            break
-          case "dynUint16":
-            switch (name) {
-              case "grouplist":
-              case "thermoseqmode":
-              case "listofattr":
-              case "bypassresult":
-                r.fwd(-1)
-                type = "len8uint16" as any
-                break
-              default:
-                throw new Error(`unknown dynUint16 name: ${name}`)
-            }
-            break
-          case "dynUint24":
-            switch (name) {
-              case "aalert":
-                r.fwd(-1)
-                type = "len8uint24" as any
-                break
-              default:
-                throw new Error(`unknown dynUint24 name: ${name}`)
-            }
-            break
-          case "dynUint32":
-            switch (name) {
-              case "logid":
-                r.fwd(-1)
-                type = "len8uint32" as any
-                break
-              default:
-                throw new Error(`unknown dynUint32 name: ${name}`)
-            }
-            break
-          case "zonebuffer":
-            r.fwd(-1)
-            break
-          case "neighborsInfo":
-            r.fwd(-1)
-            break
-        }
-        // End of dirty hacks
+    this.cluster = cluster.key
 
-        const fn =
-          specialReads[type] ||
-          readDataTable[type] ||
-          readDataTable[getStdType(type)]
+    const cmdEntry = direction
+      ? zclId.getCmdRsp(this.cluster, cmd)
+      : zclId.functional(this.cluster, cmd)
+    if (!cmdEntry) throw new Error(`Unknown command ${this.cluster}/${cmd}`)
 
-        if (!fn) throw new Error(`No read function for ${type}`)
+    this.cmd = cmdEntry.key
 
-        let out = fn(r)
+    const wholeCommand = zclId.zclmeta.functional.get(this.cluster, this.cmd)
+    if (!wholeCommand)
+      throw new Error(`Unknown command ${this.cluster}/${this.cmd}`)
+    this.cmdId = wholeCommand.id
+    this.params = wholeCommand.params as [string, ParamTypes][]
 
-        // TODO: Remove all these dirty hacks
-        if (name === "extra") {
-          Object.assign(data, out)
-          continue
-        }
-        // End of dirty hacks
-
-        data[name] = out
-      }
-      return data
+    const _direction = zclId.zclmeta.functional.getDirection(
+      this.cluster,
+      this.cmd
+    )
+    if (!_direction) {
+      throw new Error("Unrecognized direction")
     }
-
-    // args can be an array or a value-object if given
-    frame(c: BufferBuilder, args) {
-      if (typeof args !== "object")
-        throw new TypeError("`args` must be an object or array")
-      const newArgs = this.params.map(
-        Array.isArray(args)
-          ? ([, type], i) => ({ type, value: args[i] })
-          : ([name, type]) => {
-              if (!args.hasOwnProperty(name)) {
-                throw new Error("The argument object has incorrect properties")
-              }
-              return { type, value: args[name] }
-            }
-      )
-
-      frameArgs(c, newArgs)
+    if (_direction !== (direction ? "serverToClient" : "clientToServer")) {
+      throw new Error("Wrong direction")
     }
+    this.direction = _direction
   }
 
-  return FuncPayload
+  parse(r: BufferWithPointer) {
+    const data: Record<string, any> = {}
+    for (let [name, type] of this.params) {
+      // TODO: Remove all these dirty hacks
+      switch (type) {
+        case "dynUint8":
+          switch (name) {
+            case "scenelist":
+            case "zoneidlist":
+            case "zoneid":
+            case "bypassresult":
+              r.fwd(-1)
+              type = "len8uint8" as any
+              break
+            case "logpayload":
+              r.fwd(-4)
+              type = "len32uint8" as any
+              break
+            case "intervals":
+              r.fwd(-3) // len8 and attrId
+              type = "intervals" as any
+              break
+            default:
+              throw new Error(`unknown dynUint8 name: ${name}`)
+          }
+          break
+        case "dynUint16":
+          switch (name) {
+            case "grouplist":
+            case "thermoseqmode":
+            case "listofattr":
+            case "bypassresult":
+              r.fwd(-1)
+              type = "len8uint16" as any
+              break
+            default:
+              throw new Error(`unknown dynUint16 name: ${name}`)
+          }
+          break
+        case "dynUint24":
+          switch (name) {
+            case "aalert":
+              r.fwd(-1)
+              type = "len8uint24" as any
+              break
+            default:
+              throw new Error(`unknown dynUint24 name: ${name}`)
+          }
+          break
+        case "dynUint32":
+          switch (name) {
+            case "logid":
+              r.fwd(-1)
+              type = "len8uint32" as any
+              break
+            default:
+              throw new Error(`unknown dynUint32 name: ${name}`)
+          }
+          break
+        case "zonebuffer":
+          r.fwd(-1)
+          break
+        case "neighborsInfo":
+          r.fwd(-1)
+          break
+      }
+      // End of dirty hacks
+
+      const fn =
+        specialReads[type] ||
+        readDataTable[type] ||
+        readDataTable[getStdType(type)]
+
+      if (!fn) throw new Error(`No read function for ${type}`)
+
+      let out = fn(r)
+
+      // TODO: Remove all these dirty hacks
+      if (name === "extra") {
+        Object.assign(data, out)
+        continue
+      }
+      // End of dirty hacks
+
+      data[name] = out
+    }
+    return data
+  }
+
+  // args can be an array or a value-object if given
+  frame(c: BufferBuilder, args) {
+    if (typeof args !== "object")
+      throw new TypeError("`args` must be an object or array")
+    const newArgs = this.params.map(
+      Array.isArray(args)
+        ? ([, type], i) => ({ type, value: args[i] })
+        : ([name, type]) => {
+            if (!args.hasOwnProperty(name)) {
+              throw new Error("The argument object has incorrect properties")
+            }
+            return { type, value: args[name] }
+          }
+    )
+
+    frameArgs(c, newArgs)
+  }
 }
 
-export { funcPayloadFactory }
-const frameArgMap = {
+const specialWrites = {
   preLenUint8: (c: BufferBuilder, value: number) => c.uint8(value),
   preLenUint16: (c: BufferBuilder, value: number) => c.uint16le(value),
   preLenUint32: (c: BufferBuilder, value: number) => c.uint32le(value),
@@ -350,13 +346,13 @@ const frameArg = <T extends ParamTypes>(
   value: SecondArgument<
     T extends keyof typeof writeDataTable
       ? typeof writeDataTable[T]
-      : T extends keyof typeof frameArgMap
-      ? typeof frameArgMap[T]
+      : T extends keyof typeof specialWrites
+      ? typeof specialWrites[T]
       : never
   >
 ): void => {
   const fn: ((c: BufferBuilder, val: typeof value) => void) | undefined =
-    frameArgMap[type as string] || writeDataTable[getStdType(type as string)]
+    specialWrites[type as string] || writeDataTable[getStdType(type as string)]
   if (!fn) throw new Error(`No frameArgMap defined for ${type}`)
   fn(c, value)
 }
