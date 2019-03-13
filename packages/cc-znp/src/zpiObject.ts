050c93df82b5e84f7f96a62a993e23aad883fd92
qm3ster <qm3ster@gmail.com>
2019-03-13 18:29:16 +0200

    chore(cc-znp): initally convert zpiObject to TS

--- packages/cc-znp/src/zpiObject.ts
+++ packages/cc-znp/src/zpiObject.ts
@@ -1,6 +1,5 @@
-'use strict';
-const zmeta = require('./zmeta');
-const Unpi = require('unpi');
+import * as zmeta from './zmeta';
+import Unpi = require('unpi');
 const Concentrate = Unpi.Concentrate;
 const DChunks = Unpi.DChunks;
 const ru = DChunks().Rule();
@@ -12,21 +11,15 @@ const ru = DChunks().Rule();
  * args is optional, and can be an array or a value-object if given
  */
 class ZpiObject {
+  type: string;
+  subsys: string;
+  cmd: string;
+  cmdId: number;
+  args?: { name: string; type: string; value: any }[];
   constructor(subsys, cmd, args) {
     const subsystem = zmeta.Subsys.get(subsys);
     let command;
     let reqParams;
-
-    // string after assgined
-    this.type = undefined;
-    // string after assigned
-    this.subsys = undefined;
-    // string after assigned
-    this.cmd = undefined;
-    // number after assigned
-    this.cmdId = undefined;
-    // array after assigned: [ { name, type, value }, ... ]
-    this.args = undefined;
 
     if (!subsystem) {
       throw new Error('Unrecognized subsystem');
@@ -77,7 +70,6 @@ class ZpiObject {
   }
 
   parse(type, bufLen, zBuf, callback) {
-    const chunkRules = [];
     let err;
     let rspParams;
     let parser;
@@ -93,19 +85,16 @@ class ZpiObject {
     }
 
     // [ { name, type }, ... ]
-    if (rspParams) {
-      rspParams.forEach(arg => {
-        let rule = ru[arg.type];
-        if (rule) {
-          rule = rule(arg.name, bufLen);
-          chunkRules.push(rule);
-        } else {
-          err = new Error(`Parsing rule for ${arg.type} is not found.`);
-        }
-      });
-    } else {
-      err = new Error('Response parameter definitions not found.');
-    }
+    if (!rspParams)
+      return callback(new Error('Response parameter definitions not found.'));
+
+    let chunkRules;
+
+    chunkRules = rspParams.map(arg => {
+      let rule = ru[arg.type];
+      if (!rule) throw new Error(`Parsing rule for ${arg.type} is not found.`);
+      return rule(arg.name, bufLen);
+    });
 
     if (!err) {
       if (chunkRules.length === 0) {
@@ -262,7 +251,7 @@ ru.clause('devlistbuffer', function(name, bufLen) {
 ru.clause('nwklistbuffer', function(name, bufLen) {
   this.buffer(name, bufLen - 6).tap(function() {
     const buf = this.vars[name];
-    const list = [];
+    const list: ReturnType<typeof getList>[] = [];
     let listcount;
     let getList;
     let start = 0;
@@ -312,7 +301,7 @@ ru.clause('preLenBeaconlist', function(name) {
   this.uint8(name).tap(function() {
     this.buffer('preLenData', 21 * this.vars[name]).tap(function() {
       const buf = this.vars.preLenData;
-      const list = [];
+      const list: ReturnType<typeof beaconList>[] = [];
       let len = 21;
       let start = 0;
       let end = 21;
@@ -337,106 +326,117 @@ ru.clause('dynbuffer', function(name) {
 });
 
 function networkList(buf) {
-  const item = {};
   let i = 0;
 
-  item.neightborPanId = buf.readUInt16LE(i);
+  const neightborPanId = buf.readUInt16LE(i);
   i += 2 + 6;
-  item.logicalChannel = buf.readUInt8(i);
+  const logicalChannel = buf.readUInt8(i);
   i += 1;
-  item.stackProfile = buf.readUInt8(i) & 0x0f;
-  item.zigbeeVersion = (buf.readUInt8(i) & 0xf0) >> 4;
+  const stackProfile = buf.readUInt8(i) & 0x0f;
+  const zigbeeVersion = (buf.readUInt8(i) & 0xf0) >> 4;
   i += 1;
-  item.beaconOrder = buf.readUInt8(i) & 0x0f;
-  item.superFrameOrder = (buf.readUInt8(i) & 0xf0) >> 4;
+  const beaconOrder = buf.readUInt8(i) & 0x0f;
+  const superFrameOrder = (buf.readUInt8(i) & 0xf0) >> 4;
   i += 1;
-  item.permitJoin = buf.readUInt8(i);
+  const permitJoin = buf.readUInt8(i);
   i += 1;
 
-  return item;
+  return {
+    neightborPanId,
+    logicalChannel,
+    stackProfile,
+    zigbeeVersion,
+    beaconOrder,
+    superFrameOrder,
+    permitJoin,
+  };
 }
 
 function neighborLqiList(buf) {
-  const item = {};
   let i = 0;
 
-  item.extPandId = addrBuf2Str(buf.slice(0, 8));
+  const extPandId = addrBuf2Str(buf.slice(0, 8));
   i += 8;
-  item.extAddr = addrBuf2Str(buf.slice(8, 16));
+  const extAddr = addrBuf2Str(buf.slice(8, 16));
   i += 8;
-  item.nwkAddr = buf.readUInt16LE(i);
+  const nwkAddr = buf.readUInt16LE(i);
   i += 2;
-  item.deviceType = buf.readUInt8(i) & 0x03;
-  item.rxOnWhenIdle = (buf.readUInt8(i) & 0x0c) >> 2;
-  item.relationship = (buf.readUInt8(i) & 0x70) >> 4;
+  const deviceType = buf.readUInt8(i) & 0x03;
+  const rxOnWhenIdle = (buf.readUInt8(i) & 0x0c) >> 2;
+  const relationship = (buf.readUInt8(i) & 0x70) >> 4;
   i += 1;
-  item.permitJoin = buf.readUInt8(i) & 0x03;
+  const permitJoin = buf.readUInt8(i) & 0x03;
   i += 1;
-  item.depth = buf.readUInt8(i);
+  const depth = buf.readUInt8(i);
   i += 1;
-  item.lqi = buf.readUInt8(i);
+  const lqi = buf.readUInt8(i);
   i += 1;
 
-  return item;
+  return {
+    extPandId,
+    extAddr,
+    nwkAddr,
+    deviceType,
+    rxOnWhenIdle,
+    relationship,
+    permitJoin,
+    depth,
+    lqi,
+  };
 }
 
 function routingTableList(buf) {
-  const item = {};
   let i = 0;
 
-  item.destNwkAddr = buf.readUInt16LE(i);
+  const destNwkAddr = buf.readUInt16LE(i);
   i += 2;
-  item.routeStatus = buf.readUInt8(i) & 0x07;
+  const routeStatus = buf.readUInt8(i) & 0x07;
   i += 1;
-  item.nextHopNwkAddr = buf.readUInt16LE(i);
+  const nextHopNwkAddr = buf.readUInt16LE(i);
   i += 2;
 
-  return item;
+  return { destNwkAddr, routeStatus, nextHopNwkAddr };
 }
 
 function bindTableList(buf, listcount) {
-  let itemObj;
-  const list = [];
-  const len = 21;
-  let start = 0;
-  let end = len;
-  let i;
-
   function getList(buf) {
-    const itemObj = {
-      item: {},
-      thisItemLen: 0,
-    };
-    let itemLen = 21;
-    const item = {};
+    let thisItemLen = 21;
     let i = 0;
 
-    item.srcAddr = addrBuf2Str(buf.slice(0, 8));
+    const srcAddr = addrBuf2Str(buf.slice(0, 8));
     i += 8;
-    item.srcEp = buf.readUInt8(i);
+    const srcEp = buf.readUInt8(i);
     i += 1;
-    item.clusterId = buf.readUInt16LE(i);
+    const clusterId = buf.readUInt16LE(i);
     i += 2;
-    item.dstAddrMode = buf.readUInt8(i);
+    const dstAddrMode = buf.readUInt8(i);
     i += 1;
-    item.dstAddr = addrBuf2Str(buf.slice(12, 20));
+    const dstAddr = addrBuf2Str(buf.slice(12, 20));
     i += 8;
 
     // 'Addr64Bit'
-    if (item.dstAddrMode === 3) {
-      item.dstEp = buf.readUInt8(i);
+    if (dstAddrMode === 3) {
+      const dstEp = buf.readUInt8(i);
       i += 1;
-    } else {
-      itemLen = itemLen - 1;
+      return {
+        item: { srcAddr, srcEp, clusterId, dstAddrMode, dstAddr, dstEp },
+        thisItemLen,
+      };
     }
+    thisItemLen = thisItemLen - 1;
 
-    itemObj.thisItemLen = itemLen;
-    itemObj.item = item;
-    return itemObj;
+    return {
+      item: { srcAddr, srcEp, clusterId, dstAddrMode, dstAddr },
+      thisItemLen,
+    };
   }
 
-  for (i = 0; i < listcount; i += 1) {
-    itemObj = getList(buf.slice(start, end));
+  const list: ReturnType<typeof getList>['item'][] = [];
+  const len = 21;
+  let start = 0;
+  let end = len;
+  for (let i = 0; i < listcount; i += 1) {
+    const itemObj = getList(buf.slice(start, end));
     list.push(itemObj.item);
 
     start = start + itemObj.thisItemLen;
@@ -455,35 +455,47 @@ function bindTableList(buf, listcount) {
 }
 
 function beaconList(buf) {
-  const item = {};
   let i = 0;
 
-  item.srcAddr = buf.readUInt16LE(i);
+  const srcAddr = buf.readUInt16LE(i);
   i += 2;
-  item.padId = buf.readUInt16LE(i);
+  const padId = buf.readUInt16LE(i);
   i += 2;
-  item.logicalChannel = buf.readUInt8(i);
+  const logicalChannel = buf.readUInt8(i);
   i += 1;
-  item.permitJoin = buf.readUInt8(i);
+  const permitJoin = buf.readUInt8(i);
   i += 1;
-  item.routerCapacity = buf.readUInt8(i);
+  const routerCapacity = buf.readUInt8(i);
   i += 1;
-  item.deviceCapacity = buf.readUInt8(i);
+  const deviceCapacity = buf.readUInt8(i);
   i += 1;
-  item.protocolVersion = buf.readUInt8(i);
+  const protocolVersion = buf.readUInt8(i);
   i += 1;
-  item.stackProfile = buf.readUInt8(i);
+  const stackProfile = buf.readUInt8(i);
   i += 1;
-  item.lqi = buf.readUInt8(i);
+  const lqi = buf.readUInt8(i);
   i += 1;
-  item.depth = buf.readUInt8(i);
+  const depth = buf.readUInt8(i);
   i += 1;
-  item.updateId = buf.readUInt8(i);
+  const updateId = buf.readUInt8(i);
   i += 1;
-  item.extPandId = addrBuf2Str(buf.slice(13));
+  const extPandId = addrBuf2Str(buf.slice(13));
   i += 8;
 
-  return item;
+  return {
+    srcAddr,
+    padId,
+    logicalChannel,
+    permitJoin,
+    routerCapacity,
+    deviceCapacity,
+    protocolVersion,
+    stackProfile,
+    lqi,
+    depth,
+    updateId,
+    extPandId,
+  };
 }
 
 function addrBuf2Str(buf) {
@@ -504,15 +516,14 @@ function addrBuf2Str(buf) {
 }
 
 function bufToArray(buf, nip) {
-  let i;
-  const nipArr = [];
+  const nipArr: number[] = [];
 
   if (nip === 'uint8') {
-    for (i = 0; i < buf.length; i += 1) {
+    for (let i = 0; i < buf.length; i += 1) {
       nipArr.push(buf.readUInt8(i));
     }
   } else if (nip === 'uint16') {
-    for (i = 0; i < buf.length; i += 2) {
+    for (let i = 0; i < buf.length; i += 2) {
       nipArr.push(buf.readUInt16LE(i));
     }
   }
